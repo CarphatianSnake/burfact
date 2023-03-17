c6522cb4840856d6dccd5171b662a722577bd782
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-03-17 19:10:43 +0200

    docs: add comments for burger card component

--- src/components/Burgers/BurgerCard/index.js
+++ src/components/Burgers/BurgerCard/index.js
@@ -7,46 +7,48 @@ import './BurgerCard.scss';
 // Component
 function BurgerCard({ burger, setIsOpen, isOpen, index }) {
 
-  const [ isCardOpen, setIsCardOpen ] = useState(false);
-  const [ btnText, setBtnText ] = useState('Details');
-  const [ btnTextClass, setBtnTextClass ] = useState('burger-card_btn_text');
-  const [ ingredientsClass, setIngredientsClass] = useState('burger-card_ingredient display_none transparent');
+  const [ isCardOpen, setIsCardOpen ] = useState(false); // state for card open/close status
+  const [ btnText, setBtnText ] = useState('Details'); // state of button text
+  const [ btnTextClass, setBtnTextClass ] = useState('burger-card_btn_text'); // state of class names of button text
+  const [ ingredientsClass, setIngredientsClass] = useState('burger-card_ingredient display_none transparent'); // state of class names of ingredients list
 
   const { name, image, ingredient } = burger;
 
+  // Function for toggle open state of card
   function handleClick() {
     setIsCardOpen(!isCardOpen);
   }
 
   useEffect(() => {
-    const isOpenArr = [...isOpen];
-    isOpenArr[index] = isCardOpen;
-    setIsOpen(isOpenArr);
+    const isOpenArr = [...isOpen]; // clone open status array, that comes from props
+    isOpenArr[index] = isCardOpen; // set open status for card by index at cloned status array
+    setIsOpen(isOpenArr); // change state, that comes from props, for cloned status array
 
     if (isCardOpen) {
-      setBtnTextClass('burger-card_btn_text transparent');
-      setIngredientsClass('burger-card_ingredient transparent');
+      setBtnTextClass('burger-card_btn_text transparent'); // make button text transparent on start of transition
+      setIngredientsClass('burger-card_ingredient transparent'); // make ingredients list transparent on start of transition by removing display: block property
       setTimeout(() => {
-        setIngredientsClass('burger-card_ingredient');
+        setIngredientsClass('burger-card_ingredient'); // remove transparency on ingredients list for smooth transition
       }, 50)
       setTimeout(() => {
-        setBtnText('Reduce');
-        setBtnTextClass('burger-card_btn_text');
+        setBtnText('Reduce'); // Change button text on click
+        setBtnTextClass('burger-card_btn_text'); // remove transparency on button for smooth transition
       }, 250)
-    } else {
-      setBtnTextClass('burger-card_btn_text transparent');
-      setIngredientsClass('burger-card_ingredient transparent');
+    }else {
+      setBtnTextClass('burger-card_btn_text transparent'); // make button text transparent on start of transition
+      setIngredientsClass('burger-card_ingredient transparent'); // make ingredients list transparent on start of transition
       setTimeout(() => {
-        setIngredientsClass('burger-card_ingredient transparent display_none');
+        setIngredientsClass('burger-card_ingredient transparent display_none'); // hide ingredients list after transition
       }, 300)
       setTimeout(() => {
-        setBtnText('Details');
-        setBtnTextClass('burger-card_btn_text');
+        setBtnText('Details'); // Change button text on click
+        setBtnTextClass('burger-card_btn_text'); // remove transparency on button for smooth transition
       }, 250)
     }
     // eslint-disable-next-line
   }, [isCardOpen])
 
+  // Prepare ingredients items for render
   const ingredientList = ingredient.map((item, i) => {
     return (
       <li key={`${item}_${i}`} className="burger-card_ingredient_item">{item}</li>
