18c7f79a1468ab32a500b9fc4ddf0be015729de4
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-03-20 14:37:36 +0200

    feat: make header, banner, ingredients and story block for tablet view

--- src/components/TheStory/index.js
+++ src/components/TheStory/index.js
@@ -8,11 +8,9 @@ import theStoryImg from '../../assets/img/the-story.jpg';
 function TheStory() {
   return (
     <section className="the-story" id="story">
-      <div className="the-story_description">
-        <h2 className="the-story_description_heading">The Story</h2>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis id arcu vel maximus.</h3>
-        <p className='the-story_description_text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget consequat lorem. Ut nec placerat purus. Fusce vitae faucibus augue, non commodo elit. Integer risus orci, ultricies ac dapibus sed, dignissim interdum quam. Proin quis dignissim diam. Donec id magna placerat, fringilla odio et, efficitur lacus. Vivamus eu ultricies erat, eu lobortis nunc. Sed nec dignissim libero. Integer cursus leo nunc. Nulla mattis interdum nunc, sed semper turpis. Cras elit lorem, mollis ut feugiat ac, ultricies eu dolor.</p>
-      </div>
+      <h2 className="the-story_heading">The Story</h2>
+      <h3 className="the-story_small-heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis id arcu vel maximus.</h3>
+      <p className='the-story_text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget consequat lorem. Ut nec placerat purus. Fusce vitae faucibus augue, non commodo elit. Integer risus orci, ultricies ac dapibus sed, dignissim interdum quam. Proin quis dignissim diam. Donec id magna placerat, fringilla odio et, efficitur lacus. Vivamus eu ultricies erat, eu lobortis nunc. Sed nec dignissim libero. Integer cursus leo nunc. Nulla mattis interdum nunc, sed semper turpis. Cras elit lorem, mollis ut feugiat ac, ultricies eu dolor.</p>
       <img src={theStoryImg} alt="The Story" className="the-story_image" />
     </section>
   )
