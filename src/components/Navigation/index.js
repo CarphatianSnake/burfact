7f6b76a2e27fb6f7f4373eb10300201784818ac3
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-03-22 16:18:19 +0200

    feat: add smooth scrolling using nav buttons

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -12,19 +12,25 @@ function Navigation({ setIsModalOpen }) {
 
   const navSections = navSectionsNames.map((section) => {
 
+    // handler for scroll screen to elements position
+    function scrollTo() {
+      const targetScrollTo = document.getElementById(section);
+      setIsModalOpen && onModalClose(setIsModalOpen); // if setIsModalOpen comes with props we close modal, if not - ignore this line
+      targetScrollTo && targetScrollTo.scrollIntoView({behavior: 'smooth'}); // if target exists we scroll into it position using ID of section
+    }
+
     return (
       <li
         key={section}
         className='navigation_list_item'
-        onClick={() => onModalClose(setIsModalOpen)}
+        onClick={scrollTo}
       >
-        <a
-          href={`#${section}`}
+        <span
           className='navigation_list_item_link'
           title={capitalize(section)}
         >
           {section.toUpperCase()}
-        </a>
+        </span>
       </li>
     )
     
